7d2ffdaf64192bbec4c3a16cef1e2349b04ae842
dudemandando <38444209+dudemandando@users.noreply.github.com>
2018-07-31 22:27:47 -0700

    Started Add Part function, need to link to Main.java instance inventory variable

--- src/sample/AddProductController.java
+++ src/sample/AddProductController.java
@@ -9,8 +9,11 @@ import javafx.scene.control.Button;
 import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
+import sample.Part;
 
 public class AddProductController implements Initializable {
+
+    private Part partToAdd;
 
     @FXML private TextField addProductID;
     @FXML private TextField addProductName;
@@ -23,25 +26,39 @@ public class AddProductController implements Initializable {
     @FXML
     protected void addProduct(ActionEvent event){
         System.out.println("add Product button -- " + addProductID.getText());
+        partToAdd = new Part(
+                addProductName.toString(),
+                Double.parseDouble(addProductPrice.toString()),
+                Integer.parseInt(addProductInv.toString()),
+                Integer.parseInt(addProductMin.toString()),
+                Integer.parseInt(addProductMax.toString())
+        );
+
+        //Add Funcionality to Add part to inventory instance in Main.java
+
     }
 
     @FXML
     protected void deleteProduct(ActionEvent event){
+
         System.out.println("delete Product button");
     }
 
     @FXML
     protected void saveProduct(ActionEvent event){
+
         System.out.println("save product button");
     }
 
     @FXML
     protected void cancelProduct(ActionEvent event){
+
         System.out.println("cancel product");
     }
 
     @FXML
     protected void searchAddProduct(ActionEvent event){
+
         System.out.println(" search add product");
     }
 
